5ff71dd875dc2285fe403de86dad0162dc179fa6
Hardik <2019mcb1220@iitrpr.ac.in>
2021-05-23 04:48:44 +0530

    added routing to random flop components

--- src/assets/router-table.ts
+++ src/assets/router-table.ts
@@ -1,10 +1,12 @@
 import {ActorListComponent} from '../app/actor-list/actor-list.component';
 import {ActorEditComponent} from '../app/actor-edit/actor-edit.component';
 import {Error404Component} from '../app/error404/error404.component';
+import {RandomFlopComponent} from '../app/random-flop/random-flop.component'
 
 export const RouterTable=[
     {path:''     ,   redirectTo:'/actors' , pathMatch:'full'  },
     {path:'actors', component:ActorListComponent},
     {path:'actors/:id/edit',component:ActorEditComponent},
+    {path:'randomflop', component:RandomFlopComponent},
     {path:'**',component:Error404Component},
 ]
